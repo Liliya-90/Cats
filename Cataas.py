9991bc6f3e140759c52649543049e9fec2caf547
Лилия-90 <enigma_90@inbox.ru>
2025-07-28 16:38:30 +0300

    Добавлено получение изображений по тегу, а также без тега

--- Cataas.py
+++ Cataas.py
@@ -18,7 +18,10 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
+
     if img:
        new_window = Toplevel()
        new_window.title("Картинка с котиком")
@@ -28,16 +31,19 @@ def open_new_window():
        label.image = img
 
 
-
-
 def exit():
     window.destroy()
-
 
 
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
+
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 
 
